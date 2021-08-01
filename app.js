4944c41e2ee2575dd1f3036abb7176ced3e957f1
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-08-01 14:34:16 -0500

    Rendering cards

--- app.js
+++ app.js
@@ -65,20 +65,15 @@ const planets = [
   },
 ];
 
-// =============================================================
-//   WRITE YOUR CODE BELOW
-// =============================================================
 
-// 1: Create a 'Planet' component that renders a planet card
-
-
-// 2: Create a container component that iterates over the planets array 
-//    and renders a 'Planet' component for each object in the array 
-
-
-// 3: Render the container component to the DOM
-
-
+const Container = () => 
+{
+  return (
+  <div className = "container">
+    <Planet />
+  </div>
+  );
+}
 const Planet = () => {
   return (
     <div className="container">
@@ -102,6 +97,6 @@ const Planet = () => {
 
 ReactDOM.render
 (
-  <Planet />,
+  <Container />,
   document.getElementById('root')
 );
